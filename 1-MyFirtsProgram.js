bf1b2997836a651a6e7fd7e60b948bf2e45be5de
Amanda <amandabuenotozatti@gmail.com>
2022-02-18 09:26:33 -0300

    Language Introduction

---
+++ 1-MyFirtsProgram.js
@@ -0,0 +1,9 @@
+console.log("My first program: working with variables");
+
+const age = 26;
+
+console.log(age);
+console.log(age+2);
+
+const addedAges = age+2;
+console.log(addedAges);
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# JavaScript - Firsts Steps
+
+> Objective
+
+### Language Introduction
+- Using REPL in NodeJS
+- How to print values
+- Variable declaration
+- Arithmetics operators
